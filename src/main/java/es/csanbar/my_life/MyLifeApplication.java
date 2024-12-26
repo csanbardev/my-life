34f09a413194da7fe918e7797768ea6440b9df05
csanbardev <csanbar97@gmail.com>
2024-12-26 12:20:29 +0100

    initial commit

---
+++ src/main/java/es/csanbar/my_life/MyLifeApplication.java
@@ -0,0 +1,13 @@
+package es.csanbar.my_life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/csanbar/my_life/MyLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package es.csanbar.my_life;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
